4885082aa53fa9e4816058c4f65cf45b23ece06f
JayB111 <jay.ljungby@gmail.com>
2024-11-22 12:16:55 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+
+class Program
+{
+    public static void Main()
+    {
+        
+        string[] products = { "Milk", "Yoghurt", "Cream", "Cottage Cheese" };
+
+        
+        foreach (string product in products)
+        {
+            Console.WriteLine("Product: " + product);
+        }
+        Console.ReadKey();
+    }
+}
